d7af46bd5c45048472ab78b8b2bd46700a2d1600
Camilo Sanchez <111465555+LuxWise@users.noreply.github.com>
2024-12-16 11:39:19 -0500

    first commit

---
+++ src/main/java/NexoLiterario/NexoLiterario/NexoLiterarioApplication.java
@@ -0,0 +1,13 @@
+package NexoLiterario.NexoLiterario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoLiterarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoLiterarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NexoLiterario/NexoLiterario/NexoLiterarioApplicationTests.java
@@ -0,0 +1,13 @@
+package NexoLiterario.NexoLiterario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoLiterarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
